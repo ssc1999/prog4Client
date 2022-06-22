92178b47dd94257bfe6ac5702f30525b9af4214c
ssc1099 <ssc1099.csmm@gmail.com>
2022-06-22 13:33:21 +0200

    client updated

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,7 @@ void menuInicio()
 	cout << " ------------------------------------------------" << endl;
 }
 
-void menuPrograma()
+void menuProgramaComprador()
 {
 	cout << " ------------------------------------------------ " << endl;
 	cout << "| Concesionario                      | - | o | X |" << endl;
@@ -92,6 +92,18 @@ void menuContrasenya()
 	cout << " ------------------------------------------------" << endl;
 }
 
+void menuRegistro()
+{
+	cout << " ------------------------------------------------" << endl;
+	cout << "| Concesionario                      | - | o | X |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "|                                                |" << endl;
+	cout << "|   1. Comprador                                 |" << endl;
+	cout << "|   2. Vendedor                                  |" << endl;
+	cout << "|                                                |" << endl;
+	cout << " ------------------------------------------------" << endl;
+}
+
 void cajaTextoSuperior()
 {
 	cout << " ------------------------------------------------ " << endl;
@@ -116,7 +128,7 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 	struct sockaddr_in server;
 	char usuario[20], nombreComprador[20], buff[100], sendBuff[512], recvBuff[512];
 	int i, opcion, opcion2, opcion3, extra, extra2;
-	Coche* listaCoches[50];
+	Coche *listaCoches[50];
 	Ticket *ticket;
 
 	printf("\nInitialising Winsock...\n");
@@ -153,9 +165,6 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 
 	printf("Connection stablished with: %s (%d)\n", inet_ntoa(server.sin_addr),
 		   ntohs(server.sin_port));
-	
-
-	
 
 	///////////////////////////////////////////////////////
 
@@ -188,12 +197,12 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 			send(s, sendBuff, sizeof(sendBuff), 0);
 
 			recv(s, recvBuff, sizeof(recvBuff), 0);
-			if (strcmp(recvBuff, "correcto") == 0)
+			if (strcmp(recvBuff, "comprador") == 0)
 			{
 				do
 				{
 					system("cls");
-					menuPrograma();
+					menuProgramaComprador();
 					cin >> opcion2;
 					cout << "\n";
 
@@ -207,7 +216,7 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 						extra = atoi(recvBuff);
 						for (i = 0; i < extra; i++)
 						{
-							Coche* coche = new Coche();
+							Coche *coche = new Coche();
 							recv(s, recvBuff, sizeof(recvBuff), 0);
 							coche->setMatricula(recvBuff);
 							recv(s, recvBuff, sizeof(recvBuff), 0);
@@ -215,7 +224,7 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 							recv(s, recvBuff, sizeof(recvBuff), 0);
 							coche->setModelo(recvBuff);
 							recv(s, recvBuff, sizeof(recvBuff), 0);
-							extra2 = atoi(recvBuff);// esta dpm
+							extra2 = atoi(recvBuff); // esta dpm
 							coche->setAutomatico(extra2);
 							recv(s, recvBuff, sizeof(recvBuff), 0);
 							extra2 = atoi(recvBuff);
@@ -240,19 +249,19 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 						// strcpy(sendBuff, (to_string(listaCoches[opcion3]->getAnyoFabricacion())).c_str());
 						// send(s, sendBuff, sizeof(sendBuff), 0);
 
-						//coche matricula
+						// coche matricula
 						strcpy(sendBuff, listaCoches[opcion3]->getMatricula());
 						send(s, sendBuff, sizeof(sendBuff), 0);
 
-						//usuario
+						// usuario
 						strcpy(sendBuff, usuario);
 						send(s, sendBuff, sizeof(sendBuff), 0);
 
-						//fecha actual
+						// fecha actual
 						strcpy(sendBuff, "2022-06-28");
 						send(s, sendBuff, sizeof(sendBuff), 0);
 
-						//recibo el nombre del usuario
+						// recibo el nombre del usuario
 						recv(s, recvBuff, sizeof(recvBuff), 0);
 						strcpy(nombreComprador, recvBuff);
 
@@ -266,7 +275,7 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 							ticket->setMatricula(listaCoches[opcion3]->getMatricula());
 							ticket->setFechaCompra("2022-06-28");
 
-							//lo imprimo
+							// lo imprimo
 							ticket->imprimirInformacion();
 
 							cout << "Compra realizada con exito" << endl;
@@ -309,6 +318,128 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 					}
 				} while (opcion2 != 5);
 			}
+			if (strcmp(recvBuff, "vendedor") == 0)
+			{
+				do
+				{
+					system("cls");
+					menuPrograma();
+					cin >> opcion2;
+					cout << "\n";
+
+					switch (opcion2)
+					{
+					case 1:
+						// mandar instruccion comprarCoche al servidor
+						strcpy(sendBuff, "comprarCoches");
+						send(s, sendBuff, sizeof(sendBuff), 0);
+						recv(s, recvBuff, sizeof(recvBuff), 0);
+						extra = atoi(recvBuff);
+						for (i = 0; i < extra; i++)
+						{
+							Coche *coche = new Coche();
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							coche->setMatricula(recvBuff);
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							coche->setMarca(recvBuff);
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							coche->setModelo(recvBuff);
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							extra2 = atoi(recvBuff); // esta dpm
+							coche->setAutomatico(extra2);
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							extra2 = atoi(recvBuff);
+							coche->setPlazas(extra2);
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							extra2 = atoi(recvBuff);
+							coche->setAnyoFabricacion(extra2);
+							coche->imprimirInformacion();
+							listaCoches[i] = coche;
+						}
+						// introduce que coche comprar
+						cin >> opcion3;
+
+						// strcpy(sendBuff, listaCoches[opcion3]->getMarca());
+						// send(s, sendBuff, sizeof(sendBuff), 0);
+						// strcpy(sendBuff, listaCoches[opcion3]->getModelo());
+						// send(s, sendBuff, sizeof(sendBuff), 0);
+						// strcpy(sendBuff, (to_string(listaCoches[opcion3]->getAutomatico())).c_str());
+						// send(s, sendBuff, sizeof(sendBuff), 0);
+						// strcpy(sendBuff, (to_string(listaCoches[opcion3]->getPlazas())).c_str());
+						// send(s, sendBuff, sizeof(sendBuff), 0);
+						// strcpy(sendBuff, (to_string(listaCoches[opcion3]->getAnyoFabricacion())).c_str());
+						// send(s, sendBuff, sizeof(sendBuff), 0);
+
+						// coche matricula
+						strcpy(sendBuff, listaCoches[opcion3]->getMatricula());
+						send(s, sendBuff, sizeof(sendBuff), 0);
+
+						// usuario
+						strcpy(sendBuff, usuario);
+						send(s, sendBuff, sizeof(sendBuff), 0);
+
+						// fecha actual
+						strcpy(sendBuff, "2022-06-28");
+						send(s, sendBuff, sizeof(sendBuff), 0);
+
+						// recibo el nombre del usuario
+						recv(s, recvBuff, sizeof(recvBuff), 0);
+						strcpy(nombreComprador, recvBuff);
+
+						if (strcmp(recvBuff, "OK") == 0)
+						{
+
+							// creo el ticket
+							ticket = new Ticket();
+							ticket->setNomComprador(nombreComprador);
+							ticket->setNomUsuario(usuario);
+							ticket->setMatricula(listaCoches[opcion3]->getMatricula());
+							ticket->setFechaCompra("2022-06-28");
+
+							// lo imprimo
+							ticket->imprimirInformacion();
+
+							cout << "Compra realizada con exito" << endl;
+
+							// imprime ticket de compra realizada
+						}
+						else
+						{
+							cout << "Error en la compra" << endl;
+						}
+
+						cout << recvBuff << endl;
+						break;
+					case 2:
+						// mandar instruccion misCoches al servidor
+						strcpy(sendBuff, "misCoches");
+						send(s, sendBuff, sizeof(sendBuff), 0);
+
+						break;
+					case 3:
+						// mandar instruccion misTickets al servidor
+						strcpy(sendBuff, "misTickets");
+						send(s, sendBuff, sizeof(sendBuff), 0);
+
+						break;
+					case 4:
+						// mandar instruccion miPerfil al servidor
+						strcpy(sendBuff, "verPerfil");
+						send(s, sendBuff, sizeof(sendBuff), 0);
+
+						break;
+					case 5:
+						strcpy(sendBuff, "cerrarSesion");
+						send(s, sendBuff, sizeof(sendBuff), 0);
+						system("cls");
+						break;
+					default:
+						cout << "Error, seleccione otra opcion." << endl;
+						break;
+					}
+				} while (opcion2 != 5);
+			}
+
 			else
 			{
 				system("cls");
@@ -321,8 +452,28 @@ int main(int argc, char *argv[]) // se pueden meter argumentos de programa en el
 			break;
 		case 2:
 			// registro(s);
-			strcpy(sendBuff, "register");
+			strcpy(sendBuff, "registro");
 			send(s, sendBuff, sizeof(sendBuff), 0);
+			menuRegistro();
+			cin >> sendBuff;
+			send(s, sendBuff, sizeof(sendBuff), 0);
+			if (strcmp(sendBuff, "1"))
+			{
+
+				// mandar al server que queremos registrar comprador para
+				// pedir atributos por pantalla
+				// mandar atributos al server
+			}
+			if (strcmp(sendBuff, "2"))
+			{
+				// mandar al server que queremos registrar comprador para
+				// pedir atributos por pantalla
+				// mandar atributos al server
+			}
+			else
+			{
+				cout << "Introduzca un numero valido" << endl;
+			}
 			break;
 		case 3:
 			strcpy(sendBuff, "exit");
